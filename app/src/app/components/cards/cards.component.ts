fc37b5c655dafadda6cf2cd27912a9658e8e1d76
GlaucoTolotto <glaucotolotto@gmail.com>
2025-04-11 14:08:04 -0300

    Ajustando componente para Standalone

--- app/src/app/components/cards/cards.component.ts
+++ app/src/app/components/cards/cards.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../api/api.service';
 import { PokemonData } from '../../models/pokemons.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.scss',
-  providers: [ApiService], 
-  standalone: false
+  providers: [ApiService],
+  imports: [CommonModule],
 })
 export class CardsComponent {
   title = 'app';
@@ -19,7 +20,7 @@ export class CardsComponent {
     this.api.getPokemon('electivire').subscribe((res) => {
       if (res) {
         this.pokemon = res;
-        console.log(this.pokemon.sprites.front_default)
+        console.log(this.pokemon.sprites.front_default);
         this.exibir = !this.exibir;
       }
     });
