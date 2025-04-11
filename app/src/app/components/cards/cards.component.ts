6951a26cf6c0e0047a694ed56b665a6d9317e4b1
GlaucoTolotto <glaucotolotto@gmail.com>
2025-04-11 14:18:47 -0300

    Retira HTML de teste; Adiciona propriedade de data do Pokemon

--- app/src/app/components/cards/cards.component.ts
+++ app/src/app/components/cards/cards.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { ApiService } from '../../api/api.service';
+import { Component, Input } from '@angular/core';
 import { PokemonData } from '../../models/pokemons.model';
 import { CommonModule } from '@angular/common';
 
@@ -7,22 +6,8 @@ import { CommonModule } from '@angular/common';
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.scss',
-  providers: [ApiService],
   imports: [CommonModule],
 })
 export class CardsComponent {
-  title = 'app';
-  pokemon: PokemonData | undefined;
-  exibir = false;
-  constructor(private api: ApiService) {}
-
-  getPokemon() {
-    this.api.getPokemon('electivire').subscribe((res) => {
-      if (res) {
-        this.pokemon = res;
-        console.log(this.pokemon.sprites.front_default);
-        this.exibir = !this.exibir;
-      }
-    });
-  }
+  @Input() pokemon: PokemonData | undefined;
 }
