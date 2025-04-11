8f699e15675a6fdf1142ac3ebdfd1660a3951315
GlaucoTolotto <glaucotolotto@gmail.com>
2025-04-11 14:08:53 -0300

    Retirando imports desnecessarios

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AppRoutingModule } from './app.routes';
-import { ComponentsModule } from './components/components.module';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [CommonModule, AppRoutingModule, ComponentsModule],
+  imports: [CommonModule, AppRoutingModule],
 })
 export class AppComponent {}
--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CardsComponent } from './components/cards/cards.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [{ path: '', component: HomeComponent }];
