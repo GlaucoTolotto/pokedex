9163f44dc8bd5f07f841d0dc9c021e860941df3a
GlaucoTolotto <glaucotolotto@gmail.com>
2025-04-11 13:55:39 -0300

    Ajusta class para tratar erros da requisicao

--- app/src/app/pages/home/home.component.ts
+++ app/src/app/pages/home/home.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ApiService } from '../../api/api.service';
+import { PokemonData } from '../../models/pokemons.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: false,
-  providers: [ApiService]
 })
 export class HomeComponent {
-  form = this.criarForm();
+  form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private service: ApiService) {}
+  constructor(private formBuilder: FormBuilder, private service: ApiService) {
+    this.form = this.criarForm();
+  }
 
   criarForm(): FormGroup {
     return this.formBuilder.group({ pokemonName: [''] });
@@ -23,6 +25,13 @@ export class HomeComponent {
   }
 
   onSubimit() {
-    this.service.getPokemon(this.pokemonNameControl?.value).subscribe(data => console.log(data))
+    this.service.getPokemon(this.pokemonNameControl?.value).subscribe({
+      next: (data: PokemonData)=>{
+        console.log(data)
+      },
+      error: (error)=>{
+        console.log('Erro ao buscar o Pokemon', error);
+      }
+    })
   }
 }
