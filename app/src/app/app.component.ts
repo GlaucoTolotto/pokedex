02804be701eb04b0720881eb923a288fcd7943f9
GlaucoTolotto <glaucotolotto@gmail.com>
2025-04-11 14:04:04 -0300

    Retira Module da Pages

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { AppRoutingModule } from './app.routes';
 import { ComponentsModule } from './components/components.module';
 import { CommonModule } from '@angular/common';
-import { PagesModule } from './pages/pages.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [CommonModule, AppRoutingModule, ComponentsModule, PagesModule],
+  imports: [CommonModule, AppRoutingModule, ComponentsModule],
 })
 export class AppComponent {}
