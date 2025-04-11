d40ff39fba65217d0bfd3846f9661ea9b1bb8d1a
GlaucoTolotto <glaucotolotto@gmail.com>
2025-04-11 14:19:40 -0300

    Adiciona componente de card na pagina home; Adiciona propriedade para receber o data

--- app/src/app/pages/home/home.component.ts
+++ app/src/app/pages/home/home.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { ApiService } from '../../api/api.service';
 import { PokemonData } from '../../models/pokemons.model';
 import { CommonModule } from '@angular/common';
+import { CardsComponent } from '../../components/cards/cards.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
-  providers: [ApiService]
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, CardsComponent],
+  providers: [ApiService],
 })
 export class HomeComponent {
   form: FormGroup;
+  pokemon: PokemonData | undefined;
 
   constructor(private formBuilder: FormBuilder, private service: ApiService) {
     this.form = this.criarForm();
@@ -22,18 +29,18 @@ export class HomeComponent {
     return this.formBuilder.group({ pokemonName: [''] });
   }
 
-  get pokemonNameControl(){
-    return this.form.get('pokemonName')
+  get pokemonNameControl() {
+    return this.form.get('pokemonName');
   }
 
   onSubimit() {
     this.service.getPokemon(this.pokemonNameControl?.value).subscribe({
-      next: (data: PokemonData)=>{
-        console.log(data)
+      next: (data: PokemonData) => {
+        this.pokemon = data
       },
-      error: (error)=>{
+      error: (error) => {
         console.log('Erro ao buscar o Pokemon', error);
-      }
-    })
+      },
+    });
   }
 }
