3a570041c4c1ebd075fb322c4010943400217359
GlaucoTolotto <glaucotolotto@gmail.com>
2025-04-14 19:37:54 -0300

    Chamando API ao iniciar o componente principal com resource

--- app/src/app/pages/home/home.component.ts
+++ app/src/app/pages/home/home.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  inject,
+  OnInit,
+  resource,
+  ResourceRef,
+} from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -9,7 +17,9 @@ import { ApiService } from '../../api/api.service';
 import { PokemonData } from '../../models/pokemons.model';
 import { CommonModule } from '@angular/common';
 import { CardsComponent } from '../../components/cards/cards.component';
-import { AllPokemon, PokemonsList } from '../../models/allPokemons.model';
+import { AllPokemon } from '../../models/allPokemons.model';
+import { HttpClient } from '@angular/common/http';
+import {rxResource } from '@angular/core/rxjs-interop'
 
 @Component({
   selector: 'app-home',
@@ -17,21 +27,38 @@ import { AllPokemon, PokemonsList } from '../../models/allPokemons.model';
   styleUrl: './home.component.scss',
   imports: [CommonModule, FormsModule, ReactiveFormsModule, CardsComponent],
   providers: [ApiService],
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 export class HomeComponent implements OnInit {
+  private httpClient = inject(HttpClient);
   form: FormGroup;
-  pokemons: any | undefined;
+  pokemons = resource<AllPokemon, unknown>({
+    loader: async () => {
+      const response = await fetch(
+        'https://pokeapi.co/api/v2/pokemon?limit=151'
+      );
+      const data = await response.json();
+      return data.results;
+    },
+  });
+  pokemons2 = rxResource<AllPokemon, unknown>({
+    loader: () => {
+     return this.httpClient.get<AllPokemon>('https://pokeapi.co/api/v2/pokemon?limit=151')
+    },
+  });
+  // pokemons: AllPokemon | PokemonData | undefined;
 
   constructor(private formBuilder: FormBuilder, private service: ApiService) {
     this.form = this.criarForm();
   }
 
   ngOnInit(): void {
-    this.service.getAllPokemons().subscribe({
-      next: (data: AllPokemon) => {
-        this.pokemons = data.results;
-      },
-    });
+
+    // effect(() => {
+    //   if (this.rxPokemons()) {
+    //     this.pokemons = this.rxPokemons();
+    //   }
+    // });
   }
 
   criarForm(): FormGroup {
@@ -43,13 +70,13 @@ export class HomeComponent implements OnInit {
   }
 
   onSubimit() {
-    this.service.getPokemonByName(this.pokemonNameControl?.value).subscribe({
-      next: (data: PokemonData) => {
-        this.pokemons = data;
-      },
-      error: (error) => {
-        console.log('Erro ao buscar o Pokemon', error);
-      },
-    });
+    // this.service.getPokemonByName(this.pokemonNameControl?.value).subscribe({
+    //   next: (data: PokemonData) => {
+    //     this.pokemons = data;
+    //   },
+    //   error: (error) => {
+    //     console.log('Erro ao buscar o Pokemon', error);
+    //   },
+    // });
   }
 }
