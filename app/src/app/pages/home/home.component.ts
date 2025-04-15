67bc2eaa95df851dd3551a81b32d659b10a0991e
GlaucoTolotto <glaucotolotto@gmail.com>
2025-04-15 15:39:56 -0300

    Retira imposts nao utlizados

--- app/src/app/pages/home/home.component.ts
+++ app/src/app/pages/home/home.component.ts
@@ -1,16 +1,10 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ApiService } from '../../api/api.service';
 import { CommonModule } from '@angular/common';
 import { CardsComponent } from '../../components/cards/cards.component';
 import { AllPokemon } from '../../models/allPokemons.model';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { PokemonData } from '../../models/pokemons.model';
 
 @Component({
   selector: 'app-home',
@@ -25,12 +19,11 @@ export class HomeComponent {
   pokemons = rxResource<AllPokemon, { parameters: string }>({
     request: () => ({ parameters: this.pokemonSerchWordKey() }),
     loader: ({ request }) => {
-      return  this.service.getAllPokemons(request.parameters);
+      return this.service.getAllPokemons(request.parameters);
     },
   });
 
-  constructor(private service: ApiService) {
-  }
+  constructor(private service: ApiService) {}
 
   serchPokemonHandler(event: Event) {
     const searchWord = (event.target as HTMLInputElement).value;
