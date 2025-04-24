6fce5640b1c876c495feff66293f4ef50553226a
GlaucoTolotto <glaucotolotto@gmail.com>
2025-04-24 16:15:37 -0300

    adiciona import

--- app/src/app/components/pokebola8-bits/pokebola8-bits.component.ts
+++ app/src/app/components/pokebola8-bits/pokebola8-bits.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pokebola8-bits',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pokebola8-bits.component.html',
   styleUrl: './pokebola8-bits.component.scss'
 })
