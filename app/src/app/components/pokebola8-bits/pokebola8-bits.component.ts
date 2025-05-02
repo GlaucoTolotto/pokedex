eed5d2efb6815b5e140cfe4111e47b8237278175
GlaucoTolotto <glaucotolotto@gmail.com>
2025-05-02 15:28:49 -0300

    adiciona funcao para aparecer animacao

--- app/src/app/components/pokebola8-bits/pokebola8-bits.component.ts
+++ app/src/app/components/pokebola8-bits/pokebola8-bits.component.ts
@@ -1,12 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-pokebola8-bits',
   imports: [CommonModule],
   templateUrl: './pokebola8-bits.component.html',
-  styleUrl: './pokebola8-bits.component.scss'
+  styleUrl: './pokebola8-bits.component.scss',
 })
 export class Pokebola8BitsComponent {
+  animate = false;
 
+  triggerAnimation() {
+    // Ativa a classe de animação
+    this.animate = true;
+
+    // Remove a classe após a animação terminar (ex: 1s)
+    setTimeout(() => {
+      this.animate = false;
+    }, 1000); // tempo igual à duração da animação
+  }
 }
